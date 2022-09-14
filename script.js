18de9482faed5264261727e84034e43923649948
aliftareq <aliftareq@gmail.com>
2022-09-15 00:42:41 +0600

    making inter second issue problem solved

--- script.js
+++ script.js
@@ -67,7 +67,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  let timeTaken = (finishTime - startTime) / 1000;
+  timeTaken = Math.round(timeTaken)
 
   // show result modal
   resultModal.innerHTML = "";
@@ -132,7 +133,8 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  let timeSpent = (currentTime - startTime) / 1000;
+  timeSpent = Math.round(timeSpent)
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
